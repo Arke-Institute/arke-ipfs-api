4eebafdce54765d6261f89d366ae1518d0ac126d
Nick Chimicles <nac2197@columbia.edu>
2025-10-12 11:27:45 -0400

    Remove /genesis endpoint, keep only /arke

--- src/index.ts
+++ src/index.ts
@@ -134,36 +134,6 @@ app.get('/arke', async (c) => {
   }
 });
 
-// GET /genesis - Alias for /arke
-app.get('/genesis', async (c) => {
-  const ARKE_PI = '00000000000000000000000000';
-  const ipfs: IPFSService = c.get('ipfs');
-  const tipSvc: TipService = c.get('tipService');
-
-  try {
-    const tipCid = await tipSvc.readTip(ARKE_PI);
-    const manifest = (await ipfs.dagGet(tipCid)) as ManifestV1;
-
-    return c.json({
-      pi: manifest.pi,
-      ver: manifest.ver,
-      ts: manifest.ts,
-      manifest_cid: tipCid,
-      prev_cid: manifest.prev ? manifest.prev['/'] : null,
-      components: Object.fromEntries(
-        Object.entries(manifest.components).map(([label, linkObj]) => [
-          label,
-          linkObj['/'],
-        ])
-      ),
-      ...(manifest.children_pi && { children_pi: manifest.children_pi }),
-      ...(manifest.note && { note: manifest.note }),
-    });
-  } catch (error) {
-    return errorToResponse(error);
-  }
-});
-
 // 404 handler
 app.notFound((c) => {
   return c.json(
